581e491f8a72fc8403e127534b30552ce4ce60d0
hrnath88 <hrnath88@DESKTOP-POLNBOP>
2023-07-17 14:22:43 +0530

    initial commit

---
+++ helloWord/src/Shwetha/helloDemo.java
@@ -0,0 +1,18 @@
+package Shwetha;
+
+public class helloDemo {
+
+	public static void main(String[] args) {
+		char ch='S';
+		System.out.println(ch);
+		char ch1=65;
+		System.out.println(ch1);
+		char var1='\u00A7';
+		System.out.println(var1);
+		char var2='\u20AC';
+		System.out.println(var2);
+		int a='A';
+		System.out.println(a);
+		System.out.println("hello word");
+		}
+}
